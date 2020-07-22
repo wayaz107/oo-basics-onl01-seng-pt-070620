a3411a9641bf28187aa5a0fdfc74db6eed1f9803
Warda Ayaz <wayaz107@gmail.com>
2020-07-22 05:02:54 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,13 +1,13 @@
 # Make your shoe class here!
 
-class Shoe 
+class Shoe
 
 attr_accessor :brand, :color, :size, :material , :condition
 
   def initialize(brand)
-    @brand = brand 
-end 
+    @brand = brand
+end
 def cobble
   puts "The shoe has been repaired"
-def 
+def
 end 
